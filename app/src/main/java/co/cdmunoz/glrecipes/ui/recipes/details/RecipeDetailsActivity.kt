fd931d5f2da417d122f17a8b1e751085f489edf8
carlosmunoz <carlosmunoz@thepublicgroup.com>
2020-07-19 16:00:32 -0500

    Some additional behaviors and minor fixes were added

--- app/src/main/java/co/cdmunoz/glrecipes/ui/recipes/details/RecipeDetailsActivity.kt
+++ app/src/main/java/co/cdmunoz/glrecipes/ui/recipes/details/RecipeDetailsActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
+import co.cdmunoz.glrecipes.R
 import co.cdmunoz.glrecipes.data.api.ApiService
 import co.cdmunoz.glrecipes.data.api.RetrofitService
 import co.cdmunoz.glrecipes.data.model.Recipe
 import co.cdmunoz.glrecipes.data.model.RecipeDetails
 import co.cdmunoz.glrecipes.databinding.ActivityRecipeDetailsBinding
+import co.cdmunoz.glrecipes.databinding.CustomRecipeDetailsTitleBinding
 import co.cdmunoz.glrecipes.ui.recipes.RecipesViewModel
 import co.cdmunoz.glrecipes.ui.recipes.ViewModelFactory
 import co.cdmunoz.glrecipes.utils.Result
+import co.cdmunoz.glrecipes.utils.setImageFromResource
 import co.cdmunoz.glrecipes.utils.setImageFromUrl
+import kotlinx.android.synthetic.main.custom_recipe_details_title.view.*
 
 class RecipeDetailsActivity : AppCompatActivity() {
 
@@ -28,11 +32,15 @@ class RecipeDetailsActivity : AppCompatActivity() {
             layoutInflater
         )
     }
+    private val appBarTitleBinding: CustomRecipeDetailsTitleBinding by lazy {
+        CustomRecipeDetailsTitleBinding.inflate(
+            layoutInflater
+        )
+    }
     private lateinit var recipeArg: Recipe
     private val viewModel: RecipesViewModel by viewModels {
         ViewModelFactory(RetrofitService.createService(ApiService::class.java))
     }
-    private val toolbar: Toolbar by lazy { toolbar }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +48,7 @@ class RecipeDetailsActivity : AppCompatActivity() {
         loadArguments()
         initUI()
         initObservers()
-        recipeArg?.let {
+        recipeArg.let {
             viewModel.fetchRecipeDetails(it.recipeId)
         }
     }
@@ -78,9 +86,38 @@ class RecipeDetailsActivity : AppCompatActivity() {
     }
 
     private fun renderDetails(data: RecipeDetails) {
-        //toolbar.title = data.recipeTitle
-        supportActionBar?.title = data.recipeTitle
+        supportActionBar?.apply {
+            displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+            customView = appBarTitleBinding.root
+            customView.appbarTitle.text = data.recipeTitle
+        }
         binding.recipeImage.setImageFromUrl(data.imageUrl)
         binding.recipeInstructions.text = data.instructions
+        updateRatingInfo(data.rating)
+    }
+
+    private fun updateRatingInfo(rateValue: Int) {
+        with(binding){
+            when(rateValue) {
+                1 -> {
+                    rate2.setImageFromResource(R.drawable.unfilled_star)
+                    rate3.setImageFromResource(R.drawable.unfilled_star)
+                    rate4.setImageFromResource(R.drawable.unfilled_star)
+                    rate5.setImageFromResource(R.drawable.unfilled_star)
+                }
+                2 -> {
+                    rate3.setImageFromResource(R.drawable.unfilled_star)
+                    rate4.setImageFromResource(R.drawable.unfilled_star)
+                    rate5.setImageFromResource(R.drawable.unfilled_star)
+                }
+                3 -> {
+                    rate4.setImageFromResource(R.drawable.unfilled_star)
+                    rate5.setImageFromResource(R.drawable.unfilled_star)
+                }
+                4 -> {
+                    rate5.setImageFromResource(R.drawable.unfilled_star)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/co/cdmunoz/glrecipes/utils/ImageViewExtensions.kt
+++ app/src/main/java/co/cdmunoz/glrecipes/utils/ImageViewExtensions.kt
@@ -5,3 +5,7 @@ import android.widget.ImageView
 fun ImageView.setImageFromUrl(imageUrl: String) {
     GlideApp.with(context).load(imageUrl).thumbnail(0.1f).into(this)
 }
+
+fun ImageView.setImageFromResource(resourceId: Int) {
+    GlideApp.with(context).load(resourceId).thumbnail(0.1f).into(this)
+}
